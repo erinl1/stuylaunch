5f1f3adf158c67930782c322f156634e4964b3df
erinl1 <erin.lee.ny@gmail.com>
2017-12-28 16:14:08 -0500

    Add repo

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/map', to: 'pages#map'
+  get 'users/:id', to: 'users#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
