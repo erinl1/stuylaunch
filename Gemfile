f4f6c8aa3253a20c0aa819fdd7f004e62fd236cd
erinl1 <erin.lee.ny@gmail.com>
2018-01-10 18:41:51 -0500

    Heroku

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'devise'
 gem 'pg'
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+as :user do
+  get 'signin', to: 'devise/sessions#new', as: :new_user_session
+  post 'signin', to: 'devise/sessions#create', as: :user_session
+  get 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+end
   root to: 'home#index'
   get 'pages/about', to: 'pages#about'
   get 'pages/map', to: 'pages#map'
-  resources :users, only: [:show]
+  get 'profile', to: 'profile#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
